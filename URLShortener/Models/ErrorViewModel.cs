f14c13a991ce5e57d4bc7a272388f06fe4a61ba8
m1klll <mikenegor@gmail.com>
2025-08-19 19:37:06 +0400

    Убрал лишнее на данном этапе, скорректировал html шаблон.

--- URLShortener/Models/ErrorViewModel.cs
+++ URLShortener/Models/ErrorViewModel.cs
@@ -2,7 +2,5 @@ namespace URLShortener.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    
 }
